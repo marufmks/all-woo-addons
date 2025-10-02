af9a30bb4c0aec680f0f60cd4b63799d7e6f83a5
Md. Maruf Hosen Khan <marufmks5556@gmail.com>
2025-10-02 20:43:16 +0600

    first commit

---
+++ ultimate-woo-addons.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Ultimate Woo Addons
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     ultimate-woo-addons
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Ultimate_Woo_Addons
+ */
+
+// Your code starts here.
